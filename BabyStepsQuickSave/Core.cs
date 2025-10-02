1d14eef57ae6d00280b33e101a9a312ab2c0e629
Caleb Orchard <caleborchard08@gmail.com>
2025-10-01 20:53:04 -0400

    Add files via upload

--- BabyStepsQuickSave/Core.cs
+++ BabyStepsQuickSave/Core.cs
@@ -8,7 +8,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime;
 using UnityEngine;
 
-[assembly: MelonInfo(typeof(BabyStepsQuickSave.Core), "BabyStepsQuickSave", "1.0.0", "Caleb Orchard", null)]
+[assembly: MelonInfo(typeof(BabyStepsQuickSave.Core), "FlyAndQuickSave", "1.0.0", "Caleb Orchard", null)]
 [assembly: MelonGame("DefaultCompany", "BabySteps")]
 
 namespace BabyStepsQuickSave
@@ -17,13 +17,15 @@ namespace BabyStepsQuickSave
     {
         DevCheatChaperone dCC;
         DevCheatsGoHere dCGH;
+        PlayerMovement pm;
         public override void OnInitializeMelon()
         {
             GameObject go = new GameObject("DevCheatChaperone_Mod");
+            GameObject dudest = GameObject.Find("Dudest");
+
             dCC = go.AddComponent<DevCheatChaperone>();
             dCGH = go.AddComponent<DevCheatsGoHere>();
-
-            dCC.
+            pm = dudest.GetComponent<PlayerMovement>();
         }
         public override void OnUpdate()
         {
@@ -34,6 +36,30 @@ namespace BabyStepsQuickSave
             else if (Input.GetKeyDown(KeyCode.F10))
             {
                 dCGH.LoadNatePos();
+            }
+            else if (Input.GetKeyDown(KeyCode.F1))
+            {
+                if (pm == null)
+                {
+                    GameObject dudest = GameObject.Find("Dudest");
+                    pm = dudest.GetComponent<PlayerMovement>();
+                }
+                pm.ToggleFlyCam();
+            }
+
+            float scroll = Input.GetAxis("Mouse ScrollWheel");
+            if (scroll != 0 && pm != null)
+            {
+                if (Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt))
+                {
+                    if (scroll > 0f) pm.IncreaseFlyCamFOV();
+                    else pm.DecreaseFlyCamFOV();
+                }
+                else
+                {
+                    if (scroll > 0f) pm.IncreadFlyCamSpd();
+                    else pm.DecreaseFlyCamSpd();
+                }
             }
         }
     }
